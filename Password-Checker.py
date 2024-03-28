09a7c23a99a08722cdf69d2ee12ef31279fb1250
eiuol <90659480+Marl6@users.noreply.github.com>
2024-03-28 09:28:37 +0800

    second commit
    included hard category

--- Password-Checker.py
+++ Password-Checker.py
@@ -1,5 +1,6 @@
 category = input('Enter your category:\n Easy\n Medium\n Hard\n').lower()
-numbers = [1,2,3,4,5,6,7,8,9,0]
+special_chars = [
+    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", "\\", ":", ";", "'", '"', "<", ">", ".", ",", "?"]
 
 if category == "easy":
     password = input('Enter your password, must be atleast 6 characters: ')
@@ -18,5 +19,17 @@ elif category == "medium":
         
         else:
             print('Not Good!')
+    else:
+        print('Password length must be greater than 7 characters.')
+
+elif category == "hard":
+    password = input('Enter your password, must be atleast 8 characters long and must contain at least one uppercase letter, one lowercase letter, and one digit: ')
+    
+    if len(password) > 7:
+        if any(char.islower() for char in password) and any(char.isupper() for char in password) and any(char.isdigit() for char in password) and any(char in password for char in special_chars):
+                print('Good!')
+        else:
+            print('Not Good!')
+    
     else:
         print('Password length must be greater than 7 characters.')
